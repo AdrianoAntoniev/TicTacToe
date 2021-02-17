d3954f8fbc89d20bb7cfa8507ce9c3de1830a953
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2021-02-17 19:38:31 -0300

    Initial Commit

---
+++ TicTacToeSimplest/TicTacToeSimplestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSimplestApp.swift
+//  TicTacToeSimplest
+//
+//  Created by Adriano Rodrigues Vieira on 17/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSimplestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
