6f43791e1033719136ae2f6506de21d6fdce46e6
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2021-02-18 10:58:07 -0300

    Initial commit. Simple layout with grid

--- TicTacToeSimplest/ContentView.swift
+++ TicTacToeSimplest/ContentView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var grid: [String] = []
+    private var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LazyVGrid(columns: gridItemLayout, spacing: 50) {
+            ForEach(0..<9) { index in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.yellow)
+                    
+                    Text(Mark.o)
+                        .foregroundColor(.red)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        
+                }
+                
+            }
+        }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ TicTacToeSimplest/Mark.swift
@@ -0,0 +1,16 @@
+//
+//  Mark.swift
+//  TicTacToeSimplest
+//
+//  Created by Adriano Rodrigues Vieira on 18/02/21.
+//
+
+import Foundation
+
+struct Mark {
+    static let x = "X"
+    static let o = "o"
+    static let blank = ""
+    
+    private init() {}
+}
